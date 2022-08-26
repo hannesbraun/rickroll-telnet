fa45d3fbf82ec1910e3186813c1a9c6ed0321783
Hannes Braun <hannesbraun@mail.de>
2022-08-26 18:16:01 +0200

    Allow port specification

--- src/bin/net.rs
+++ src/bin/net.rs
@@ -1,12 +1,21 @@
 use rickroll_telnet::{record, replay};
 use std::net::TcpListener;
 use std::sync::Arc;
-use std::thread;
+use std::{env, thread};
 
 fn main() {
+    let port: u16 = env::args()
+        .collect::<Vec<String>>()
+        .get(1)
+        .map_or(23, |p| p.parse().unwrap_or(23));
+
     let recording = Arc::new(record());
-    let listener = TcpListener::bind("0.0.0.0:23").expect("Unable to create TCP socket");
+    let listener =
+        TcpListener::bind(format!("0.0.0.0:{}", port)).expect("Unable to create TCP socket");
+
+    // Say hi
     println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+
     for stream in listener.incoming().flatten() {
         let recording = Arc::clone(&recording);
         thread::spawn(move || {
